0208254bc63587ab92158c603bd256f285e30a32
AbdEl-Rahman21 <abdel292004@gmail.com>
2024-08-28 20:39:31 +0300

    Reorder scripts

--- scripts/homescript.js
+++ scripts/homescript.js
@@ -1,9 +1,3 @@
-let users = [
-    {uname: "user1", email: "u1@test.com", pass: "user123"},
-    {uname: "Jane Doe", email: "jane.doe@example.com", pass: "jane456"},
-    {uname: "Alex Smith", email: "alex.smith@example.com", pass: "alex789"}
-];
-
 let username = localStorage.getItem("uname");
 
 if (username == null) { // Not Logged in
---
+++ scripts/loginscript.js
@@ -0,0 +1,5 @@
+let users = [
+    {uname: "user1", email: "u1@test.com", pass: "user123"},
+    {uname: "Jane Doe", email: "jane.doe@example.com", pass: "jane456"},
+    {uname: "Alex Smith", email: "alex.smith@example.com", pass: "alex789"}
+];
