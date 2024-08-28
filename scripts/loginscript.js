38e816c2f0a468398c9947923e39d6a66a358e5d
Mohanad&Seif <mohanadhussein536@gmail.com>
2024-08-28 21:39:26 +0300

    add login script

--- scripts/loginscript.js
+++ scripts/loginscript.js
@@ -3,3 +3,20 @@ let users = [
     {uname: "Jane Doe", email: "jane.doe@example.com", pass: "jane456"},
     {uname: "Alex Smith", email: "alex.smith@example.com", pass: "alex789"}
 ];
+
+ var button = document.querySelector('form');
+ button.addEventListener("submit", checkUser);
+function checkUser(event){
+    event.preventDefault();
+    console.log("success");
+    var usersInput = document.getElementById('email').value;
+    var passInput = document.getElementById('password').value;
+    users.forEach(t => {
+        if(t.email == usersInput && t.pass == passInput){
+            localStorage.setItem("username", t.uname);
+            location.href ='../index.html';
+           
+        }
+    })
+
+}
